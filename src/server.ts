33fffbd2288ecfed5c7b463c000ba28f21a2b1e0
Jadir Junior <jadirjsjunior@gmail.com>
2021-08-12 18:10:21 -0300

    initial config

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import './util/module-alias';
+
+import * as database from '@src/database';
+
+import express, { Application } from 'express';
+
+import { Server } from '@overnightjs/core';
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+
+  public async init(): Promise<void> {
+    this.setupExpress();
+    this.setupControllers();
+    await this.databaseSetup();
+  }
+
+  private setupExpress(): void {
+    this.app.use(express.json());
+  }
+
+  private setupControllers(): void {
+    // const controller = new Controller();
+    // this.addControllers([controller])
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
+  }
+}
---
+++ test/jest-setup.ts
@@ -0,0 +1,12 @@
+import { SetupServer } from '@src/server';
+import supertest from 'supertest';
+
+let server: SetupServer;
+
+beforeAll(() => {
+  server = new SetupServer();
+  server.init();
+  global.testRequest = supertest(server.getApp());
+});
+
+afterAll(async () => await server.close());
